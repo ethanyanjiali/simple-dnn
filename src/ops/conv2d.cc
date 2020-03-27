7648f5f4ce6be1c71a6c22b321c4eb8345b2d975
Ethan Li <yanjia.li@snapchat.com>
2020-03-26 23:06:00 -0700

    add load_weights

--- src/ops/conv2d.cc
+++ src/ops/conv2d.cc
@@ -8,3 +8,10 @@ MatrixXd Conv2D::compute(MatrixXd input)
     // TODO: Implement this
     return input;
 }
+
+bool Conv2D::load_weights(MatrixXd weights, MatrixXd bias)
+{
+    this->weights = weights;
+    this->bias = bias;
+    return true;
+}
--- src/ops/conv2d.h
+++ src/ops/conv2d.h
@@ -8,9 +8,13 @@ private:
     std::tuple<int, int> kernel;
     std::tuple<int, int> padding;
     int strides;
+    MatrixXd weights;
+    MatrixXd bias;
 
 public:
     Conv2D(int filters, std::tuple<int, int> kernel, std::tuple<int, int> padding, int strides)
         : filters(filters), kernel(kernel), padding(padding), strides(strides) {}
+
     MatrixXd compute(MatrixXd input);
+    bool load_weights(MatrixXd weights, MatrixXd bias);
 };
